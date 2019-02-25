fd9686858095207ebd5c1ea17477b4449ff2385b
eramiz <eramiz@HS-D210-05.MOHS.local>
2019-02-25 13:22:54 -0500

    dasdsad

--- Lesson 3-simpleString/src/l3exercise.java
+++ Lesson 3-simpleString/src/l3exercise.java
@@ -6,11 +6,12 @@ public class l3exercise {
 
 		System.out.println("a\\b\\c\\\"d\"");
 		System.out.println("\\t is a tab. \\n is a new line, \\\" prints a \"quote\" ");
+		System.out.println("\"line one\"\n\\line       two\\");
 		System.out.println("\\\\denotes a single line comment\n\\*denotes a multiline comment*\\");
 		System.out.println(" \"\\\\\" \"\\\"\" \n \\\"\\\"\"\\ \n\\\"\"\"\"\\");
 		
 		System.out.println();
-		
+		String word5 = "if SUBSTRING is used";
 		String word = "birthday";
 		System.out.println(word.toUpperCase());
 		
@@ -18,7 +19,7 @@ public class l3exercise {
 		System.out.println(word1.substring(0,3)+" a "+word1.substring(8,13).toUpperCase());
 		
 		String word2 = "whose a whats it";
-		System.out.println(word2.substring(0,5).toUpperCase()+"\n is " +word2.substring(8,12).toUpperCase());
+		System.out.println(word2.substring(0,5).toUpperCase()+"\n"+word5.substring(12,15) +word2.substring(8,12).toUpperCase());
 	
 		String word3 = "Every Way The Wind Blows";
 		System.out.println();
@@ -26,7 +27,18 @@ public class l3exercise {
 		
 		System.out.println();
 		String word4 = "word has 4 letters";
-		System.out.println("\""+word4.substring(0,4).length()+"\""+);
+		System.out.println("\""+word4.substring(0,4).length()+"\""+word4.substring(10,18)+word4.substring(4,9)+"\""+word4.substring(0,9).length()+"\""+word4.substring(10,18));
+		System.out.println();
+		String word7 = "Peter Piper Pickled A Peck Of Picked Peppers";
+		System.out.println(word7.substring(19, 36)+"          "+word7.substring(36,44)+" "+word7.substring(0,11)+"    "+word7.substring(28,36));
+		
+		System.out.println();
+		
+		System.out.println(word5.substring(2,12).toLowerCase()+word5.substring(12,16)+word5.substring(2,11).length());
+		
+		System.out.println();
+		String word6 = "Jim, Jacky, Johny, Jill";
+		System.out.println(word6.substring(0,3).toLowerCase()+"\\"+word6.substring(0,3).length()+"\n"+word6.substring(5,10).toUpperCase()+"\\"+word6.substring(5,10).length()+"\n"+word6.substring(12,17).toLowerCase()+"\\"+word6.substring(0,6).length()+"\n"+word6.substring(19,23).toUpperCase()+"\\"+word6.substring(19,23).length());
 		
 	}
 
