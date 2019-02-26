0620c9f2ee62b165420f79cf52db9dc5b019bb19
eramiz <eramiz@HS-D210-05.MOHS.local>
2019-02-26 12:12:55 -0500

    Commit

--- Lesson 3-simpleString/src/l3exercise.java
+++ Lesson 3-simpleString/src/l3exercise.java
@@ -8,7 +8,7 @@ public class l3exercise {
 		System.out.println("\\t is a tab. \\n is a new line, \\\" prints a \"quote\" ");
 		System.out.println("\"line one\"\n\\line       two\\");
 		System.out.println("\\\\denotes a single line comment\n\\*denotes a multiline comment*\\");
-		System.out.println(" \"\\\\\" \"\\\"\" \n \\\"\\\"\"\\ \n\\\"\"\"\"\\");
+		System.out.println(" \"\\\\\" \"\\\"\" \n           \\\"\\\"\"\\ \n\\\"\"\"\"\\");
 		
 		System.out.println();
 		String word5 = "if SUBSTRING is used";
@@ -16,21 +16,22 @@ public class l3exercise {
 		System.out.println(word.toUpperCase());
 		
 		String word1 = "it's my party";
-		System.out.println(word1.substring(0,3)+" a "+word1.substring(8,13).toUpperCase());
+		System.out.println(word1.substring(0,3)+" a "+"\""+word1.substring(8,13).toUpperCase()+"\"");
 		
 		String word2 = "whose a whats it";
-		System.out.println(word2.substring(0,5).toUpperCase()+"\n"+word5.substring(12,15) +word2.substring(8,12).toUpperCase());
+		System.out.println(word2.substring(0,5).toUpperCase()+"\n"+word5.substring(13,16) +word2.substring(8,12).toUpperCase());
 	
 		String word3 = "Every Way The Wind Blows";
 		System.out.println();
-		System.out.println(word3.substring(0,4).toLowerCase()+word3.substring(5,9).toUpperCase()+word3.substring(9,13).toLowerCase()+"\\\\"+word3.substring(13,18)+"\\"+word3.substring(18,24).toLowerCase()+" 4");
+		System.out.println(word3.substring(0,4).toLowerCase()+word3.substring(5,9).toUpperCase()+word3.substring(9,13).toLowerCase()+" \\"+word3.substring(14,18).toUpperCase()+"\\"+word3.substring(18,24).toLowerCase()+" 4");
 		
 		System.out.println();
 		String word4 = "word has 4 letters";
 		System.out.println("\""+word4.substring(0,4).length()+"\""+word4.substring(10,18)+word4.substring(4,9)+"\""+word4.substring(0,9).length()+"\""+word4.substring(10,18));
 		System.out.println();
-		String word7 = "Peter Piper Pickled A Peck Of Picked Peppers";
-		System.out.println(word7.substring(19, 36)+"          "+word7.substring(36,44)+" "+word7.substring(0,11)+"    "+word7.substring(28,36));
+		
+		String word7 = "Peter Piper Pickled A Peck Of Pickel Peppers";
+		System.out.println(word7.substring(19, 36).toLowerCase()+"          "+word7.substring(36,44).toLowerCase()+" "+word7.substring(0,11)+"    "+word7.substring(29,35).toLowerCase()+"d");
 		
 		System.out.println();
 		
--- Lesson 4/src/numericVariables.java
+++ Lesson 4/src/numericVariables.java
@@ -2,13 +2,18 @@
 public class numericVariables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		
+		int a = 100;
+		int b = 200;
+		b/=a;
+		System.out.println(b+1);
 		
+		int p = 45;
+		int v = 30;
 		
-		
-		
+	
+	System.out.println(p-=30+--v);
+	System.out.println(v);
 		
 	}
 
