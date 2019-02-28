2d9aa0c28c2587cb46ff211b132cfef8c9134226
eramiz <eramiz@HS-D210-05.MOHS.local>
2019-02-28 14:20:14 -0500

    C

--- Lesson 4/src/numericVariables.java
+++ Lesson 4/src/numericVariables.java
@@ -3,17 +3,20 @@ public class numericVariables {
 
 	public static void main(String[] args) {
 		
-		int a = 100;
-		int b = 200;
-		b/=a;
-		System.out.println(b+1);
-		
-		int p = 45;
-		int v = 30;
-		
 	
-	System.out.println(p-=30+--v);
-	System.out.println(v);
+		
+		int j, b, c;
+		j = 79+3*(4+82-68)-7+19;
+		b = (179 +21+10)/7+181;
+		c = 10389*56*11+2246;
+		
+		String h,k,l;
+		h = "79+3*(4+82-68)-7+19";
+		k ="(179 +21+10)/7+181";
+		l ="10389*56*11+2246";
+		System.out.println(h+" = "+j);
+		System.out.println(k+" = "+b);
+		System.out.println(l+" = " +c);
 		
 	}
 
